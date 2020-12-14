bb49f568caeb6285014e0f1e77313df50040c9e9
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2020-12-14 13:02:48 +0100

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+export default {
+  components: true,
+  buildModules: ["@nuxtjs/tailwindcss"],
+  storybook: {
+    stories: [],
+    webpackFinal(config) {
+      return config;
+    },
+  },
+};
